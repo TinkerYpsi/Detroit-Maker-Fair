d93c58d94336f0388a31f03ef92bba721cea526f
Michael Ploof <michael@tinkertech.io>
2018-08-01 15:26:51 -0400

    Big rewrite. Stuff works.

--- Mega/Mega.ino
+++ Mega/Mega.ino
@@ -1,10 +1,11 @@
+#include <TimerOne.h>
 #include "pitches.h"
-#include <TT_RFID.h>
 #include <EEPROM.h>     // We are going to read and write PICC's UIDs from/to EEPROM
 #include <SPI.h>        // RC522 Module uses SPI protocol
-#include <MFRC522.h>  // Library for Mifare RC522 Devices
 #include <Arduino.h>
-#include <TT_Adafruit_NeoPixel.h>
+#include <Adafruit_NeoPixel.h>
+#include <MFRC522.h>
+
 
 // necessary for Adafruit Neopixels
 #ifdef __AVR__
@@ -13,254 +14,554 @@
 
 #define PIN 44
 #define NUMPIXELS 45
-#define WAIT  30
+
+constexpr uint8_t RST_PIN = 5;     // Configurable, see typical pin layout above
+constexpr uint8_t SS_PIN = 53;     // Configurable, see typical pin layout above
+
+MFRC522 mfrc522(SS_PIN, RST_PIN);   // Create MFRC522 instance
 
 constexpr uint8_t wipeB = 3;     // Button pin for WipeMode
 bool programMode = false;  // initialize programming mode to false
 bool successRead;    // Variable integer to keep if we have Successful Read from Reader
-byte cardID[4];   // Stores scanned ID read from RFID Module
-TT_RFID rfid;
 
-TT_Adafruit_NeoPixel strip = TT_Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
 
 // for laser harp
+const int LASER_THRESHOLD = 130;
 int speakerPin = 3;
-int duration = 300;
 
-int laser1 = A0;
-int laser2 = A1;
-int laser3 = A2;
-int laser4 = A3;
-int laser5 = A4;
-int laser6 = A5;
-int laser7 = A6;
-int laser8 = A7;
-int allLasers[8] = {laser1, laser2, laser3, laser4, laser5, laser6, laser7, laser8};
-int *trigLaser;
+const int LASER_COUNT = 8;
+const int LASER[LASER_COUNT] = {A0, A1, A2, A3, A4, A5, A6, A7};
 
-int val1;
-int val2;
-int val3;
-int val4;
-int val5;
-int val6;
-int val7;
-int val8;
+int laser_val[8] = {0};
 
 int tone1 = NOTE_C4;
 int tone2 = NOTE_D4;
 int tone3 = NOTE_E4;
-int tone4 = NOTE_G4;
-int tone5 = NOTE_A4;
-int tone6 = NOTE_B4;
-int tone7 = NOTE_D5;
-int tone8 = NOTE_F5;
+int tone4 = NOTE_F4;
+int tone5 = NOTE_G4;
+int tone6 = NOTE_A4;
+int tone7 = NOTE_B4;
+int tone8 = NOTE_C5;
 int allTones[8] = {tone1, tone2, tone3, tone4, tone5, tone6, tone7, tone8};
 
 // for racing LEDs
-int first_player;
-int second_player;
-const int wait = 30;
-const int button1 = 40;
-const int button2 = 42;
-int position1 = 0;          // where player 1 starts the race
-int position2 = NUMPIXELS;  // where player 2 starts the race
-int color1 = RED;
-int color2 = BLUE;
+const int WAIT_TIME = 30;
+const int BUTTON1 = 40;
+const int BUTTON2 = 41;
+
+const int JOY_BUTTON = 47;
+const int JOY_X = A14;
+const int JOY_Y = A15;
+
+
 
 int IR_pin = A8;
 int bigRedSwitch = 30;   // TODO: IMPLEMENT
 // TODO: add serial reading from Arduino w/ LCD
 
-int* minVal(int *allVals[]);
-
 void setup()
 {
-  Serial.begin(9600);
-  // This is for Trinket 5V 16MHz, you can remove these three lines if you are not using a Trinket
-  #if defined (__AVR_ATtiny85__)
-    if (F_CPU == 16000000) clock_prescale_set(clock_div_1);
-  #endif
-  // End of trinket special code
-
+  Serial.begin(115200);
+  
   strip.begin();
   strip.show(); // Initialize all pixels to 'off'
 
   pinMode(wipeB, INPUT_PULLUP);   // Enable pin's pull up resistor
-  //TODO: CHECK IF RFID LIB ONLY INITIALIZES PROPER PINS FOR UNO & NOT MEGA
-   rfid.initialize();
-   Serial.begin(9600);  // Initialize serial communications with PC
-   rfid.showReaderDetails();  // Show details of Card Reader
-   rfid.toggleDeleteAllRecords(wipeB);
-   bool masterDefined = rfid.isMasterDefined();
-   if(!masterDefined) {
-     rfid.defineMasterCard();
-   }
-   rfid.printInitMessage();
+//  //TODO: CHECK IF RFID LIB ONLY INITIALIZES PROPER PINS FOR UNO & NOT MEGA
+//   rfid.initialize();
+//   rfid.showReaderDetails();  // Show details of Card Reader
+//   rfid.toggleDeleteAllRecords(wipeB);
+//   bool masterDefined = rfid.isMasterDefined();
+//   if(!masterDefined) {
+//     rfid.defineMasterCard();
+//   }
+//   rfid.printInitMessage();
 
-  pinMode(button1, INPUT_PULLUP);
-  pinMode(button2, INPUT_PULLUP);
+  SPI.begin();                                                  // Init SPI bus
+  mfrc522.PCD_Init();                                              // Init MFRC522 card
+  
+
+  pinMode(BUTTON1, INPUT_PULLUP);
+  pinMode(BUTTON2, INPUT_PULLUP);
   pinMode(bigRedSwitch, INPUT);
   pinMode(IR_pin, INPUT);
+  
+  Timer1.initialize(10000);         // initialize timer1, and set a 1/2 second period
+  Timer1.attachInterrupt(checkInputs);  // attaches callback() as a timer overflow interrupt
+  
   Serial.println("finished running setup");
+
+  
 }
+
+typedef enum _mode{
+  RACE,
+  HARP,
+  DISTANCE,
+  JOYSTICK,
+  RFID
+}MODE;
+
+MODE mode = RFID;
 
 void loop()
 {
-
-  // IR LED CODE
-  int IR_val = analogRead(IR_pin);
-  // someone's hand is over the sensor
-  if(IR_val < 1000)
+  if(Serial.available())
   {
-    int pixelsToLight = (IR_val / 1023) * 45;
-    for(int i = 0; i < pixelsToLight; i++)
+    delay(15);
+    while(Serial.available())
     {
-      strip.setPixelColor(i, TEAL);
+      char input = Serial.read();
+      if(input == 'r'){
+        mode = RACE;
+        Serial.println("Race");
+        clearPixels();
+        strip.show();
+      }
+      else if(input == 'h'){
+        mode = HARP;
+        Serial.println("Harp");
+        clearPixels();
+        strip.show();
+      }
+      else if(input == 'd'){
+        mode = DISTANCE;
+        Serial.println("Distance Sensor");
+        clearPixels();
+        strip.show();
+      }
+      else if(input == 'j'){
+        mode = JOYSTICK;
+        Serial.println("Joystick");
+        clearPixels();
+        strip.show();
+      }
+      else if(input == 'i'){
+        mode = RFID;
+        Serial.println("RFID");
+        clearPixels();
+        strip.show();
+      }
+    }
+  }
+  switch(mode)
+  {
+    case RACE:
+    {
+      runRace();
+      break;
+    }
+    case HARP:
+    {
+      runHarp();
+      break;
+    }
+    case DISTANCE:
+    {
+      runDistanceSensor();
+      break;
+    }
+    case JOYSTICK:
+    {
+      runJoystick();
+      break;
+    }
+    case RFID:
+    {
+      runRFID();
+      break;
+    }
+    default:
+    {
+      break;
+    }
+  }
+}
+
+void checkInputs(){
+  
+}
+
+void runJoystick(){
+  int x_val = 0;
+  int y_val = 0;
+  bool button_val = digitalRead(JOY_BUTTON);
+  const int SAMPLES = 20;
+  
+  for(int i = 0; i < SAMPLES; i++)
+  {
+    x_val += analogRead(JOY_X);
+    y_val += analogRead(JOY_Y);
+  }
+  x_val /= SAMPLES;
+  y_val /= SAMPLES;
+
+
+  int pixel = map(x_val, 0, 800, 0, 45);
+  long color = Wheel(map(y_val, 0, 800, 0, 255));
+  clearPixels();
+  strip.setPixelColor(pixel, color);
+  strip.show();
+
+  char output[255];
+  sprintf(output, "X: %i  Y: %i  B: %i", x_val, y_val, button_val);
+  Serial.println(output);
+  delay(100);
+}
+
+void runDistanceSensor(){
+// IR LED CODE
+  while(!Serial.available())
+  {
+    const int SAMPLES = 20;
+    int ir_val = 0;
+    for(int i = 0; i < SAMPLES; i++){
+      ir_val += analogRead(IR_pin);
+    }
+    ir_val /= SAMPLES;
+  
+    Serial.print("ir_val: ");
+    Serial.println(ir_val);
+    int pixels_to_light = map(ir_val, 540, 170, 0, 45);
+  
+    for(int i = 0; i < NUMPIXELS; i++)
+    {
+      if(i < pixels_to_light){
+        strip.setPixelColor(i, strip.Color(15, 35, 25));
+      }
+      else{
+        strip.setPixelColor(i, strip.Color(0, 0, 0));
+      }
     }
     strip.show();
-    strip.begin();  // TODO: figure out how to unlight pixels
   }
+}
 
+void runHarp(){
   // LASER HARP CODE
   // TODO: INCLUDE INTERRUPT FOR LASER HARP
-  val1 = analogRead(laser1);
-  val2 = analogRead(laser2);
-  val3 = analogRead(laser3);
-  val4 = analogRead(laser4);
-  val5 = analogRead(laser5);
-  val6 = analogRead(laser6);
-  val7 = analogRead(laser7);
-  val8 = analogRead(laser8);
-  int allVals[8] = {val1, val2, val3, val4, val5, val6, val7, val8};
-  trigLaser = minVal(allVals);
+  for(int i = 0; i < LASER_COUNT; i++){
+    int this_val = analogRead(LASER[i]);
+    laser_val[i] = this_val;
+//    Serial.print("Laser ");
+//    Serial.print(i);
+//    Serial.print(": ");
+//    Serial.println(this_val);
+  }
+  int trig_laser = minIndex(laser_val);
+//  Serial.print("Active laser: ");
+//  Serial.println(trig_laser);
+//  Serial.println("");
 
-  for(int i = 0; i < 8; i++)
+  if(trig_laser >= 0){
+    tone(speakerPin, allTones[trig_laser], 50);
+  }
+  else
   {
-    if(*trigLaser == allLasers[i])
-    {
-      tone(speakerPin, allTones[i], duration);
-      delay(duration);
-      noTone(1);
-    }
+    noTone(1);
   }
+}
 
+void dump_byte_array(byte *buffer, byte bufferSize) {
+    for (byte i = 0; i < bufferSize; i++) {
+        Serial.print(buffer[i] < 0x10 ? " 0" : " ");
+        Serial.print(buffer[i], HEX);
+    }
+}
 
-  // RFID CODE
-  successRead = rfid.foundID();
-  rfid.getCardID(cardID);
-  rfid.toggleDeleteMasterCard(wipeB);
-  if(!successRead)
+unsigned long getUIDVal(){
+  unsigned long ret = 0;
+  for(int i = 0; i < 4; i++){
+    ret |= mfrc522.uid.uidByte[i];
+    ret = ret << 8;
+  }
+  return ret;
+}
+
+void runRFID(){
+  while(!Serial.available())
   {
-    return;
-  }
-  // mode for adding and deleting RFID tags
-  if (programMode)
-  {
-    // When in program mode check First If master card scanned again to exit program mode
-    if ( rfid.isMaster(cardID) )
+    const int TAG_COUNT = 3;
+    unsigned long id[TAG_COUNT] = {0};
+  
+    for(int i = 0; i < TAG_COUNT; i++)
     {
-      Serial.println(F("Master Card Scanned"));
-      Serial.println(F("Exiting Program Mode"));
-      Serial.println(F("-----------------------------"));
-      programMode = false;
-      return;
+      Serial.print("Present key ");
+      Serial.println(i);
+      while(1)
+      {
+        if(Serial.available())
+        {
+          return;
+        }
+        if(mfrc522.PICC_IsNewCardPresent())
+        {
+          if(mfrc522.PICC_ReadCardSerial()){
+            unsigned long thisUID = getUIDVal();
+            bool is_new = true;
+            for(int j = 0; j < TAG_COUNT; j++){
+              if(id[j] == thisUID)
+              {
+                is_new = false;
+              }
+            }
+            if(is_new)
+            {
+              id[i] = thisUID;
+              Serial.print("id ");
+              Serial.print(i);
+              Serial.print(": ");
+              Serial.println(id[i]);
+              int pixels_per_inc = strip.numPixels() / TAG_COUNT;
+              for(int j = 0; j < pixels_per_inc; j++)
+              {
+                strip.setPixelColor((j + i * pixels_per_inc), strip.Color(0, 0, 40));
+                strip.show();
+                delay(15);
+                if(i == TAG_COUNT - 1 && j == pixels_per_inc - 1 && ((j + i * pixels_per_inc) < strip.numPixels() - 1))
+                {
+                  strip.setPixelColor(strip.numPixels() - 1, strip.Color(0, 0, 40));
+                  strip.show();
+                }
+              }
+              Serial.println("SLDJFLJWEF:EJ");
+              break;
+            }
+          }
+        }
+      }
     }
-    else
+
+    while(!Serial.available())
     {
-      if ( rfid.findID(cardID) )
-      { // If scanned card is known delete it
-        Serial.println(F("I know this ID, removing..."));
-        rfid.deleteID(cardID);
-        Serial.println(F("Succesfully removed ID"));
-        Serial.println("-----------------------------");
-        Serial.println(F("Scan an RFID tag to ADD or REMOVE"));
+      unsigned long lastUID = 0;
+      int command = -1;
+      // Look for new cards
+      if (mfrc522.PICC_IsNewCardPresent() && mfrc522.PICC_ReadCardSerial())
+      {
+        long thisUID = getUIDVal();
+        if(lastUID != thisUID)
+        {
+          for(int i = 0; i < TAG_COUNT; i++)
+          {
+            if(id[i] == thisUID)
+            {
+              command = i;
+            }
+          }
+        }
       }
-      else
-      {                    // If scanned card is not known add it
-        Serial.println(F("I do not know this ID, adding..."));
-        rfid.writeID(cardID);
-        Serial.println(F("Succesfully added ID"));
-        Serial.println(F("-----------------------------"));
-        Serial.println(F("Scan an RFID tag to ADD or REMOVE"));
+      
+      switch(command)
+      {
+        case(0):
+        {
+          knightRider(15);
+          colorWipe(0, 25);
+          break;
+        }
+        case(1):
+        {
+          theaterChase(Wheel(random(0, 255)), 25);
+          colorWipe(0, 25);
+          break;
+        }
+        case(2):
+        {
+          rainbowCycle(5);
+          colorWipe(0, 25);
+          break;
+        }
+        case(3):
+        { 
+          colorWipe(Wheel(random(0, 255)), 25);
+          colorWipe(0, 25);
+          break;
+        }
       }
     }
   }
+}
 
-  // mode for checking IDs to see if they match existing records
-  else {
-    if ( rfid.isMaster(cardID)) {    // If scanned card's ID matches Master Card's ID - enter program mode
-      programMode = true;
-      strip.theaterChase(PURPLE, WAIT);
-      Serial.println(F("Hello Master - Entered Program Mode"));
-      Serial.println();
-      Serial.println(F("Scan an RFID tag to ADD or REMOVE"));
-      Serial.println(F("Scan Master Card again to Exit Program Mode"));
-      Serial.println(F("-----------------------------"));
-    }
-    else {
-      if ( rfid.findID(cardID) ) { // If not, see if the card is in the records
-        strip.theaterChase(GREEN, WAIT);
-        Serial.println(F("Welcome, You shall pass"));
-      }
-      else {      // If not, show that the ID was not valid
-        strip.theaterChase(RED, WAIT);
-        Serial.println(F("You shall not pass"));
-      }
-    }
-  }
-
-
+void runRace(){
   // RACING LEDS CODE
-  first_player = digitalRead(button1);
-  second_player = digitalRead(button2);
-  while(position1 < position2)
+  int position1 = 0;          // where player 1 starts the race
+  int position2 = NUMPIXELS;  // where player 2 starts the race
+  bool last_p1 = HIGH;
+  bool last_p2 = HIGH;
+  long color1 = strip.Color(40, 0, 0);
+  long color2 = strip.Color(0, 0, 40);
+  
+  while(position1 < NUMPIXELS /2 && position2 > NUMPIXELS /2)
   {
+    bool p1 = digitalRead(BUTTON1);
+    bool p2 = digitalRead(BUTTON2);
     // both players pressed button simulataneously
-    if(first_player == LOW && second_player == LOW)
+    if((!p1 && last_p1) && (!p2 && last_p2))
     {
       strip.setPixelColor(position1, color1);
       ++position1;
       strip.setPixelColor(position2, color2);
       --position2;
     }
-    else if(first_player == LOW)
+    else if((!p1 && last_p1))
     {
       strip.setPixelColor(position1, color1);
       ++position1;
     }
-    else if(second_player == LOW)
+    else if((!p2 && last_p2))
     {
       strip.setPixelColor(position2, color2);
       --position2;
+    }
+    strip.show();
+    last_p1 = p1;
+    last_p2 = p2;
+    if(Serial.available())
+    {
+      clearPixels();
+      strip.show();
+      break;
     }
   }
 
   // if player 1 won
-  if(position1 > (NUMPIXELS / 2))
+  if(position1 > (NUMPIXELS - position2))
   {
-    strip.theaterChase(color1, wait);
+    theaterChase(color1, 50);
   }
   // if player 2 won
   else
   {
-    strip.theaterChase(color2, wait);
+    theaterChase(color2, 50);
   }
-
 }
 
-int* minVal(int allVals[])
-{
-  int lowestVal = 1023;
-  int *lowestLaser;
-  int *ptr = allVals;
-  for(; ptr < ptr + 8; ++ptr)
-  {
-      if(*ptr < lowestVal)
-      {
-        lowestVal = *ptr;
-        lowestLaser = ptr;
-      }
+void clearPixels(){
+  for(int i = 0; i < strip.numPixels(); i++){
+    strip.setPixelColor(i, strip.Color(0, 0, 0));
   }
-  return ptr;
+}
+
+// Fill the dots one after the other with a color
+void colorWipe(uint32_t c, uint8_t wait) {
+  for(uint16_t i=0; i<strip.numPixels(); i++) {
+    strip.setPixelColor(i, c);
+    strip.show();
+    delay(wait);
+  }
+}
+
+//Theatre-style crawling lights.
+void theaterChase(uint32_t c, uint8_t wait) {
+  for (int j=0; j<10; j++) {  //do 10 cycles of chasing
+    for (int q=0; q < 3; q++) {
+      for (uint16_t i=0; i < strip.numPixels(); i=i+3) {
+        strip.setPixelColor(i+q, c);    //turn every third pixel on
+      }
+      strip.show();
+
+      delay(wait);
+
+      for (uint16_t i=0; i < strip.numPixels(); i=i+3) {
+        strip.setPixelColor(i+q, 0);        //turn every third pixel off
+      }
+    }
+  }
+}
+
+void knightRider(uint8_t wait){
+  const int ON_COUNT = 9;
+  bool dir = true;
+  for(int k = 0; k < 4; k++)
+  {
+    for(int i = 0; i < strip.numPixels() - ON_COUNT + 1; i++)
+    {
+      clearPixels();
+      int brightness = 0;
+      for(int j = 0; j < ON_COUNT; j++)
+      {
+        if(j == 0) brightness = 5;
+        if(j == 1) brightness = 25;
+        if(j == 2) brightness = 50;
+        if(j == 3) brightness = 100;
+        if(j == 4) brightness = 250;
+        if(j == 5) brightness = 100;
+        if(j == 6) brightness = 50;
+        if(j == 7) brightness = 25;
+        if(j == 8) brightness = 5;
+        Serial.println(brightness);
+        int pixel = dir ? i + j : (strip.numPixels()) - 1 - (i + j);
+        strip.setPixelColor(pixel, strip.Color(brightness, 0, 0));
+      }
+      strip.show();
+      delay(wait);
+      Serial.println("");
+    }
+    dir = !dir;
+  }
+}
+
+// Input a value 0 to 255 to get a color value.
+// The colours are a transition r - g - b - back to r.
+uint32_t Wheel(byte WheelPos) {
+  WheelPos = 255 - WheelPos;
+  if(WheelPos < 85) {
+    return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+  }
+  if(WheelPos < 170) {
+    WheelPos -= 85;
+    return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+  }
+  WheelPos -= 170;
+  return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+}
+
+
+// Slightly different, this makes the rainbow equally distributed throughout
+void rainbowCycle(uint8_t wait) {
+  uint16_t i, j;
+
+  for(j=0; j<256; j++) { // 1 cycle of all colors on wheel
+    for(i=0; i< strip.numPixels(); i++) {
+      strip.setPixelColor(i, Wheel(((i * 256 / strip.numPixels()) + j) & 255));
+    }
+    strip.show();
+    delay(wait);
+  }
+}
+
+//Theatre-style crawling lights with rainbow effect
+void theaterChaseRainbow(uint8_t wait) {
+  for (int j=0; j < 256; j++) {     // cycle all 256 colors in the wheel
+    for (int q=0; q < 3; q++) {
+      for (uint16_t i=0; i < strip.numPixels(); i=i+3) {
+        strip.setPixelColor(i+q, Wheel( (i+j) % 255));    //turn every third pixel on
+      }
+      strip.show();
+
+      delay(wait);
+
+      for (uint16_t i=0; i < strip.numPixels(); i=i+3) {
+        strip.setPixelColor(i+q, 0);        //turn every third pixel off
+      }
+    }
+  }
+}
+
+int minIndex(int* values)
+{
+  int lowest_val = 1023;
+  int lowest_index = 0;
+  for(int i = 0; i < LASER_COUNT; i++){
+    if(values[i] < lowest_val){
+      lowest_val = values[i];
+      lowest_index = i;
+    }
+  }
+  if(lowest_val > LASER_THRESHOLD){
+    lowest_index = -1;
+  }
+  return lowest_index;
 }
