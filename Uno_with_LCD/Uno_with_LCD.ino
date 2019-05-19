9e63962be5b493f5da7ad507d8f9984dfc4b972e
mploof <michael@tinkertech.io>
2019-05-18 20:24:36 -0400

    Switched from digital pin signalling to software serial input

--- Uno_with_LCD/Uno_with_LCD.ino
+++ Uno_with_LCD/Uno_with_LCD.ino
@@ -11,9 +11,6 @@
 #define btnNONE   5
 
 int humidity_pin = 2;
-int down_input = 11;
-int interruptPin = 3;
-int up_input = 12;
 int mega_output = 13;
 int i = 0;
 String selected_option;
@@ -32,86 +29,108 @@ AM2302 dht(humidity_pin);
 
 const int SERIAL_TX_PORT = 13;
 const int SERIAL_RX_PORT = 0;
+const int SERIAL_TX_PORT2 = 11;
+const int SERIAL_RX_PORT2 = 12;
 SoftwareSerial mySerial(SERIAL_RX_PORT, SERIAL_TX_PORT);
+SoftwareSerial mySerial2(SERIAL_RX_PORT2, SERIAL_TX_PORT2);
 
 void setup()
 {
   Serial.begin(115200);
-  pinMode(humidity_pin, INPUT_PULLUP);
-  pinMode(down_input, INPUT);
-  pinMode(interruptPin, INPUT_PULLUP);
-  attachInterrupt(digitalPinToInterrupt(interruptPin), read_external_LCD_buttons, FALLING);
-  pinMode(up_input, INPUT);
+  pinMode(humidity_pin, INPUT_PULLUP);  
   pinMode(mega_output, OUTPUT);
-  lcd.begin(16,2);
-  lcd.setCursor(0,0);
+  lcd.begin(16, 2);
+  lcd.setCursor(0, 0);
   lcd.print(options[0]);
   Serial.println(options[0]);
   lcd.cursor();
   mySerial.begin(9600);
+  mySerial2.begin(9600);
 }
 
 void loop()
 {
-  switch(btnClicked)
-  {
-    case btnUP:
-    {
-      humidity_mode = false;
-      lcd.clear();
-      // if i is less than size of options array
-      if(i + 1 < sizeof(options)/sizeof(String))
-      {
-        lcd.print(options[i + 1]);
-        Serial.println(options[i + 1]);
-        i++;
-      }
-      else
-      {
-        i = 0;
-        lcd.print(options[i]);
-        Serial.println(options[i]);
-      }
-      lcd.cursor();
-      btnClicked = btnNONE;
-      break;
+
+  if (mySerial2.available()) {
+    delay(10);
+    char in_chars[50] = {'\0'};
+    int i = 0;
+    while (mySerial2.available()) {
+      in_chars[i] = mySerial2.read();
+      i++;
     }
-    case btnDOWN:
-    {
-      humidity_mode = false;
-      lcd.clear();
-      if(i > 0)
-      {
-        lcd.print(options[i - 1]);
-        Serial.println(options[i - 1]);
-        i--;
-      }
-      else
-      {
-        // options[i] is last element
-        i = sizeof(options) / sizeof(String) - 1;
-        lcd.print(options[i]);
-      }
-      lcd.cursor();
-      btnClicked = btnNONE;
-      break;
+    Serial.print("Incoming message: ");
+    String in_str = String(in_chars);
+    Serial.println(in_str);
+
+    if (in_str.startsWith("u")) {
+      btnClicked = btnUP;
     }
-    case btnSELECT:
-    {
-      humidity_mode = false;
-      lcd.noCursor();
-      selected_option = options[i];
-      btnClicked = btnNONE;
-      break;
+    else if (in_str.startsWith("d")) {
+      btnClicked = btnDOWN;
     }
-    case btnNONE:
-    {
-      // do nothing
-      break;
+    else if (in_str.startsWith("s")) {
+      btnClicked = btnSELECT;
     }
   }
 
-  if(prev_selected_option != selected_option)
+  switch (btnClicked)
+  {
+    case btnUP:
+      {
+        humidity_mode = false;
+        lcd.clear();
+        // if i is less than size of options array
+        if (i + 1 < sizeof(options) / sizeof(String))
+        {
+          lcd.print(options[i + 1]);
+          Serial.println(options[i + 1]);
+          i++;
+        }
+        else
+        {
+          i = 0;
+          lcd.print(options[i]);
+          Serial.println(options[i]);
+        }
+        lcd.cursor();
+        break;
+      }
+    case btnDOWN:
+      {
+        humidity_mode = false;
+        lcd.clear();
+        if (i > 0)
+        {
+          lcd.print(options[i - 1]);
+          Serial.println(options[i - 1]);
+          i--;
+        }
+        else
+        {
+          // options[i] is last element
+          i = sizeof(options) / sizeof(String) - 1;
+          lcd.print(options[i]);
+        }
+        lcd.cursor();
+        break;
+      }
+    case btnSELECT:
+      {
+        humidity_mode = false;
+        lcd.noCursor();
+        selected_option = options[i];
+        break;
+      }
+    case btnNONE:
+      {
+        // do nothing
+        break;
+      }      
+  }
+  btnClicked = btnNONE;
+
+  if (prev_selected_option != selected_option)
   {
     switchMode(selected_option);
     prev_selected_option = selected_option;
@@ -119,14 +138,14 @@ void loop()
 
   static long last_check = millis();
   static bool clearedScreen = false;
-  if(humidity_mode)
+  if (humidity_mode)
   {
-    if(!clearedScreen)
+    if (!clearedScreen)
     {
       lcd.clear();
       clearedScreen = true;
     }
-    if(millis() - last_check > 3000)
+    if (millis() - last_check > 3000)
     {
       last_check = millis();
       Serial.print("last check: ");
@@ -142,16 +161,16 @@ void loop()
         return;
       }
 
-      if(btnClicked != btnNONE)
+      if (btnClicked != btnNONE)
       {
         humidity_mode = false;
       }
 
-      lcd.setCursor(0,0);   // set cursor to 1st column, 1st row
+      lcd.setCursor(0, 0);  // set cursor to 1st column, 1st row
       lcd.print("Humidity: ");
       lcd.print(dht.humidity); lcd.print("%");
       Serial.print(dht.humidity); Serial.print(" %\t\t");
-      lcd.setCursor(0,1);   // set cursor to 1st column, 2nd row
+      lcd.setCursor(0, 1);  // set cursor to 1st column, 2nd row
       lcd.print("Temp: ");
       lcd.print(dht.temperature_F); lcd.print("F");
       Serial.print(dht.temperature_F); Serial.print(" *F\n");
@@ -163,120 +182,61 @@ void loop()
 void switchMode(String selected_option)
 {
   // Race
-  if(selected_option == options[0])
+  if (selected_option == options[0])
   {
     mySerial.write("r");
   }
 
   // Laser harp
-  else if(selected_option == options[1])
+  else if (selected_option == options[1])
   {
     mySerial.write("h");
   }
 
   // RFID
-  else if(selected_option == options[2])
+  else if (selected_option == options[2])
   {
     mySerial.write("i");
   }
 
   // Temp/Humidity sensor
-  else if(selected_option == options[3])
+  else if (selected_option == options[3])
   {
     humidity_mode = true;
   }
 
   // Distance sensor
-  else if(selected_option == options[4])
+  else if (selected_option == options[4])
   {
     mySerial.write("d");
   }
 
   // Big red switch
-  else if(selected_option == options[5])
+  else if (selected_option == options[5])
   {
     mySerial.write("s");
   }
 
   // Joystick
-  else if(selected_option == options[6])
+  else if (selected_option == options[6])
   {
     mySerial.write("j");
   }
 }
 
-// read external read_onboard_LCD_buttons
-// int read_external_LCD_buttons()
-// {
-//   int up = HIGH;
-//   int click = HIGH;
-//   int down = HIGH;
-//
-//   up = digitalRead(up_input);
-//   click = digitalRead(click_input);
-//   down = digitalRead(down_input);
-//
-//   static int prev_up_state = up;
-//   static int prev_click_state = click;
-//   static int prev_down_state = down;
-//
-//   if(click == LOW && prev_click_state == HIGH)
-//   {
-//     return btnSELECT;
-//   }
-//   else if(down == LOW && prev_down_state == HIGH)
-//   {
-//     return btnDOWN;
-//   }
-//   else if(up == LOW && prev_up_state == HIGH)
-//   {
-//     return btnUP;
-//   }
-//   else
-//   {
-//     return btnNONE;
-//   }
-//
-//   prev_up_state = up;
-//   prev_click_state = click;
-//   prev_down_state = down;
-// }
-//
-void read_external_LCD_buttons()
-{
-  int up = digitalRead(up_input);
-  int down = digitalRead(down_input);
-  if(up == LOW && down == HIGH)
-  {
-    btnClicked = btnUP;
-  }
-  else if(up == HIGH && down == LOW)
-  {
-    btnClicked = btnDOWN;
-  }
-  else if(up == LOW && down == LOW)
-  {
-    btnClicked = btnSELECT;
-  }
-  else
-  {
-    btnClicked = btnNONE;
-  }
-}
-
 int read_onboard_LCD_buttons()
 {
- int adc_key_in = analogRead(0);      // read the value from the sensor
- // my buttons when read are centered at these values: 0, 144, 329, 504, 741
- // we add approx 50 to those values and check to see if we are close
+  int adc_key_in = analogRead(0);      // read the value from the sensor
+  // my buttons when read are centered at these values: 0, 144, 329, 504, 741
+  // we add approx 50 to those values and check to see if we are close
 
- if (adc_key_in < 50)   return btnRIGHT;
- if (adc_key_in < 195)  return btnUP;
- if (adc_key_in < 380)  return btnDOWN;
- if (adc_key_in < 555)  return btnLEFT;
- if (adc_key_in < 790)  return btnSELECT;
+  if (adc_key_in < 50)   return btnRIGHT;
+  if (adc_key_in < 195)  return btnUP;
+  if (adc_key_in < 380)  return btnDOWN;
+  if (adc_key_in < 555)  return btnLEFT;
+  if (adc_key_in < 790)  return btnSELECT;
 
 
 
- return btnNONE;  // when all others fail, return this...
+  return btnNONE;  // when all others fail, return this...
 }
